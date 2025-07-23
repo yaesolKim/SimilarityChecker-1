7aae254f7ce46d8fb0647c03338fed3c950d3eb3
yaesolKim <kimyaesol@gmail.com>
2025-07-23 15:53:20 +0900

    [refactoring] early return

--- Project6/similarity.cpp
+++ Project6/similarity.cpp
@@ -6,21 +6,19 @@ using std::vector;
 class Similarity {
 public:
 	int lengthScore(std::string a, std::string b) {
-		int score = 0;
-		if (a.length() == b.length()) score = 60;
-		if (a.length() >= b.length() * 2 || b.length() >= a.length() * 2) score = 0;
+		if (a.length() == b.length()) return 60;
+		if (a.length() >= b.length() * 2 || b.length() >= a.length() * 2) return 0;
 
 		else {
 			if (a.length() > b.length()) {
 				double gap = (a.length() - b.length()) / b.length();
-				score = static_cast<int>((1 - gap) * 60);
+				return static_cast<int>((1 - gap) * 60);
 			}
 			else {
 				double gap = b.length() - a.length();
-				score = static_cast<int>((1 - gap / a.length()) * 60);
+				return static_cast<int>((1 - gap / a.length()) * 60);
 
 			}
 		}
-		return score;
 	}
 };
