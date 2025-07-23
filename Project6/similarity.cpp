adcb677c3bb86ae4e8ec2d24df7b0d052d39a859
yaesolKim <kimyaesol@gmail.com>
2025-07-23 15:56:49 +0900

    [refactoring] early return

--- Project6/similarity.cpp
+++ Project6/similarity.cpp
@@ -9,16 +9,14 @@ public:
 		if (a.length() == b.length()) return 60;
 		if (a.length() >= b.length() * 2 || b.length() >= a.length() * 2) return 0;
 
-		else {
-			if (a.length() > b.length()) {
-				double gap = (a.length() - b.length()) / b.length();
-				return static_cast<int>((1 - gap) * 60);
-			}
-			else {
-				double gap = b.length() - a.length();
-				return static_cast<int>((1 - gap / a.length()) * 60);
-
-			}
+		
+		if (a.length() > b.length()) {
+			double gap = (a.length() - b.length()) / b.length();
+			return static_cast<int>((1 - gap) * 60);
 		}
+
+		double gap = b.length() - a.length();
+		return static_cast<int>((1 - gap / a.length()) * 60);
+
 	}
 };
